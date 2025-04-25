9d2d7b6d838ebcc9422d2d28f1d9ddda04364399
jingwon.lee <jingwon.lee@timf.co.kr>
2025-04-26 01:58:03 +0900

    user-service : securityFIlterChain 추가

--- user-service/src/main/java/com/jingwon/study/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/jingwon/study/userservice/config/SecurityConfig.java
@@ -3,13 +3,28 @@ package com.jingwon.study.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> {}) // enable default cors configuration
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/signup/**", "/login/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){

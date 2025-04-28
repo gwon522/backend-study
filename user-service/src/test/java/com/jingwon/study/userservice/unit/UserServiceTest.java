31a92bcdf68476bfd9aa2a218b76563f5d1fb2bd
jingwon.lee <jingwon.lee@timf.co.kr>
2025-04-28 22:56:23 +0900

    user-service : 유저서비스 테스트코드 짜보기2( createLocalUser)

--- user-service/src/test/java/com/jingwon/study/userservice/unit/UserServiceTest.java
+++ user-service/src/test/java/com/jingwon/study/userservice/unit/UserServiceTest.java
@@ -1,25 +1,30 @@
 package com.jingwon.study.userservice.unit;
 
+import com.jingwon.study.userservice.domain.User;
+import com.jingwon.study.userservice.domain.UserRole;
+import com.jingwon.study.userservice.dto.LocalSignupRequest;
 import com.jingwon.study.userservice.repository.UserRepository;
 import com.jingwon.study.userservice.service.UserService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
-
+    @Mock
+    private PasswordEncoder passwordEncoder;
     @InjectMocks
     private UserService userService;
 
@@ -40,5 +45,22 @@ public class UserServiceTest {
         String newEmail = "unique@test.com";
         // when & then
         assertDoesNotThrow(() -> userService.checkDuplicateEmail(newEmail));
+    }
+
+    @Test
+    @DisplayName("로컬 유저 생성 시 비밀번호 인코딩 및 저장이 정상 동작해야 한다")
+    void should_create_local_user_successfully() {
+        // given
+        LocalSignupRequest request = new LocalSignupRequest("jinkwon@test.com", "testPassword", "이진권", UserRole.CUSTOMER);
+        String verificationToken = "sample-token";
+
+        // mock passwordEncoder.encode()
+        when(passwordEncoder.encode("testPassword")).thenReturn("encodedPassword");
+
+        // when
+        userService.createLocalUser(request, verificationToken);
+
+        // then
+        verify(userRepository).save(any(User.class));
     }
 }
